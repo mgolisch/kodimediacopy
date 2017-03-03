650006bf090057bdbe4f451d38a9e1af66c5108a
Michael Golisch <mgolisch@googlemail.com>
2017-03-03 12:31:55 +0100

    changed ENV vars for DATABSES

--- kodimediacopy/default_settings.py
+++ kodimediacopy/default_settings.py
@@ -1,9 +1,9 @@
 import os
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', None)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', None)
 SQLALCHEMY_BINDS = {
-    'kodi':        os.environ.get('KODI_DATABASE_URI', None)
+    'kodi':        os.environ.get('KODI_DATABASE_URL', None)
 }
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 ADMIN_TOKEN = os.environ.get('ADMIN_TOKEN', None)
