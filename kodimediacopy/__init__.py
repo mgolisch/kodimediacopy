c12aa81d9fc64a2593bdff70758ae01ee869d33f
Michael Golisch <mgolisch@googlemail.com>
2017-07-11 20:44:07 +0200

    add mail related config

--- kodimediacopy/__init__.py
+++ kodimediacopy/__init__.py
@@ -2,6 +2,7 @@ import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -12,8 +13,22 @@ if app.config['SQLALCHEMY_DATABASE_URI'] is None:
     raise Exception('app database not configured')
 if app.config['SQLALCHEMY_BINDS']['kodi'] is None:
     raise Exception('kodi database not configured')
+#check flask-mail related configs
+if app.config['MAIL_SERVER'] is None:
+    raise Exception('MAIL_SERVER not configured')
+if app.config['MAIL_PORT'] is None:
+    raise Exception('MAIL_PORT not configured')
+if app.config['MAIL_USERNAME'] is None:
+    raise Exception('MAIL_USERNAME not configured')
+if app.config['MAIL_PASSWORD'] is None:
+    raise Exception('MAIL_PASSWORD not configured')
+if app.config['MAIL_DEFAULT_SENDER'] is None:
+    raise Exception('MAIL_DEFAULT_SENDER not configured')
+
+
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+mail = Mail(app)
 
 stream_handler = logging.StreamHandler()
 app.logger.addHandler(stream_handler)
--- kodimediacopy/default_settings.py
+++ kodimediacopy/default_settings.py
@@ -7,3 +7,11 @@ SQLALCHEMY_BINDS = {
 }
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 ADMIN_TOKEN = os.environ.get('ADMIN_TOKEN', None)
+
+MAIL_SERVER = os.environ.get('MAIL_SERVER', None)
+MAIL_PORT = os.environ.get('MAIL_PORT', None)
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', False)
+MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', False)
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME', None)
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', None)
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', None)
